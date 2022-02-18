f5f385d033e328dfb4ebaac3939e65374236998c
Aleksandra Los <oluchna@hotmail.com>
2022-02-18 13:26:21 +0100

    add main css

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,13 +1,15 @@
 
 import {GiWallet} from 'react-icons/gi';
 import{BsFillArrowRightCircleFill} from 'react-icons/bs';
+import Wallet from '../pictures/wallet.png';
+
 const StartPage = () => {
   return (
     <>
     <section>
       <p><GiWallet/> E-wallet</p>
       <header>
-        <img src="" alt="image" />
+        <img src={Wallet} alt="image" />
       </header>
       <h1>Easy way to manage your paymant</h1>
       <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
