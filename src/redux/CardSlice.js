ab2ddb55307c0bc6e0615ba65423c56c98ca5dc8
oscaroguzhan <72919227+oscaroguzhan@users.noreply.github.com>
2022-02-21 21:49:07 +0100

    update cardDetails into object

--- src/redux/CardSlice.js
+++ src/redux/CardSlice.js
@@ -1,18 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const CardSlice = createSlice({
-  name: "cards",
+  name: "card",
   initialState: {
     // define initial global states
-    cardDetails: [
-      {
-        number: "060766767006",
-        name: "ozzy",
-        expiry: "",
-        cvc: "",
-        focused: "",
-      },
-    ],
+    
+    cardDetails: {
+      number:'6666777788889999',
+      name:'Ozzy',
+      expiry:'202211',
+      cvc:'786',
+      focused:''
+    }
   },
   reducers: {
     //TODO: define actions
