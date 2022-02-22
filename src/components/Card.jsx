af4958fd72d11711432a6fc45f8fb88dce5878a9
Aleksandra Los <oluchna@hotmail.com>
2022-02-22 09:03:37 +0100

    add som changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
 import '../styles/cardStyle.css';
 import { IoIosAddCircle } from "react-icons/io";
+
 const Card = () => {
   return (
     <>
@@ -11,10 +12,10 @@ const Card = () => {
             className="card__front-logo card__logo"
             src="https://cdn.freelogovectors.net/wp-content/uploads/2019/02/swedbank-logo.png"
           />
-          <p className="card_numer">**** **** **** 6258</p>
+          <p className="card_numer">1234 1234 1234 6258</p>
           <div className="card__space-75">
             <span className="card__label">Card holder</span>
-            <p className="card__info">John Doe</p>
+            <p className="card__info">Ann Andresson</p>
           </div>
           <div className="card__space-25">
             <span className="card__label">Expires</span>
@@ -26,7 +27,7 @@ const Card = () => {
           <div className="card__black-line"></div>
           <div className="card__back-content">
             <div className="card__secret">
-              <p className="card__secret--last">420</p>
+              <p className="card__secret--last">123</p>
             </div>
             <img
               className="card__back-square card__square"
