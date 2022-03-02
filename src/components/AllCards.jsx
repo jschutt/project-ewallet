abbf94b8750b8e909d96f0f39167df6891f51b3b
oscaroguzhan <72919227+oscaroguzhan@users.noreply.github.com>
2022-03-03 00:35:41 +0100

    attempt to solve activecard

--- src/components/AllCards.jsx
+++ src/components/AllCards.jsx
@@ -3,17 +3,17 @@ import { useSelector, useDispatch } from "react-redux";
 import Card from "./Card";
 import "../assets/styles/StyledAddCard.css";
 import { setActive } from "../redux/CardSlice";
-import { BsTrashFill } from "react-icons/bs";
-import '../assets/styles/StyledAllCards.css';
+import { IconButton } from "@material-ui/core";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import "../assets/styles/StyledAllCards.css";
 const AllCards = () => {
-  // get the cardList state from the redux store so we can map through and put into the array
+  // get the List of cards from the redux store so we can map through and put into the array
   const { cards } = useSelector((state) => state.cards);
   let dispatch = useDispatch();
   return (
     <div>
       <div>
-        <h3>My active card</h3>
-        {cards.slice(0, 0).map((card, i) => {
+        {cards.slice(0, 1).map((card, i) => {
           return (
             <div key={i}>
               <Card
@@ -29,27 +29,28 @@ const AllCards = () => {
       </div>
       <hr />
       <div>
-        <h1>Other cards</h1>
-        {
-          cards
-            .slice(0, 3)
-            .map((card, i) => (
-              <div key={i} className="other-cards">
-                <div onClick={() => dispatch(setActive(card))}>
-                  <Card
-                    key={i}
-                    name={card.cardHolderName}
-                    number={card.cardNumber}
-                    expiry={card.expiry}
-                    cvc={card.cvc}
-                  />
-                </div>
-                <button className="trash-icon">
-                  <BsTrashFill />
-                </button>
-              </div>
-            ))}
+        <div className="header">
+          <h1>Other cards</h1>
+        </div>
+
+        {cards.slice(1, 4).map((card, i) => (
+          <div key={i} className="other-cards">
+            <div onClick={() => dispatch(setActive(card))}>
+              <Card
+                key={i}
+                name={card.cardHolderName}
+                number={card.cardNumber}
+                expiry={card.expiry}
+                cvc={card.cvc}
+              />
+            </div>
+            <IconButton >
+              <DeleteForeverIcon className="delete-icon"/>
+            </IconButton>
+          </div>
+        ))}
       </div>
+      
     </div>
   );
 };
